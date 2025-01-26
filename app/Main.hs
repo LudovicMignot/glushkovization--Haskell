1e1e2f168e7eebe5684f2e42777a143e0be4e308
Ludovic Mignot <mignotludovic@gmail.com>
2025-01-26 11:07:23 +0100

    init ajout dynamic

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo #-}
 
 module Main (main) where
 
--- import Test (runStabilization)
-
-import Control.Monad (join, void)
+import Control.Monad (void)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.ByteString as BS
 import Data.GraphViz.Commands
@@ -18,24 +18,37 @@ import qualified Data.Map as Map
 import Data.Text as Te
   ( Text,
     pack,
-    unpack,
   )
 import qualified Data.Text.Lazy as Tel
 import Language.Javascript.JSaddle.Warp (run)
-import NFA (NFA (NFA), generateNFA)
+import NFA (NFA, generateNFA)
+import NFAAccessibility (trim)
 import NFADotRepr (nfaToDot)
+import NFAHomogeneity (makeHomogeneous)
+import NFAStandard (makeStandard)
 import Reflex.Dom.Core
+  ( DomBuilder,
+    EventName (Click),
+    MonadHold (holdDyn),
+    MonadWidget,
+    Reflex (Event),
+    domEvent,
+    el,
+    elAttr,
+    elAttr',
+    elDynHtml',
+    foldDyn,
+    mainWidgetWithHead,
+    text,
+    (=:),
+  )
+import Reflex.Dom.Widget
 import ToString (ToString (toString))
-
--- import Reflex.Dom.Core (run)
-
--- main :: IO ()
--- main = runStabilization
 
 main :: IO ()
 main = run 3911 $ mainWidgetWithHead header body
 
-header :: (MonadWidget t m) => m ()
+header :: (DomBuilder t m) => m ()
 header =
   elAttr
     "link"
@@ -43,36 +56,40 @@ header =
     $ return ()
   where
     bootstrapCSSCDN =
-      "https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
 
 body :: (MonadWidget t m) => m ()
 body = do
-  aut <- liftIO $ generateNFA ['a' .. 'e'] [(0 :: Int) .. 10] 2 2 5
   _ <- elAttr "div" ("class" =: "container") $ do
     elAttr "h1" ("class" =: "text-center") $ text "Word Automata Constructions"
     el "hr" $ return ()
-
-  _ <-
-    elAttr
-      "div"
-      ("class" =: "d-flex flex-column align-items-center")
-      $ svgAut aut
+  aut <- liftIO $ generateNFA ['a' .. 'e'] [(0 :: Int) .. 10] 2 2 5
+  rec aut_dyn <- foldDyn (\_ auto -> trim auto) aut evt
+      _ <-
+        dyn $
+          ( elAttr
+              "div"
+              ("class" =: "d-flex flex-column align-items-center")
+              . svgAut
+          )
+            <$> aut_dyn
+      evt <- labelledButton "Make Standard"
   footer
 
-footer :: (MonadWidget t m) => m ()
+footer :: (DomBuilder t m) => m ()
 footer = do
-  elAttr "script" (Map.fromList [("defer", "defer"), ("src", jqueryCDN)]) $
-    return ()
-  elAttr "script" (Map.fromList [("defer", "defer"), ("src", popperCDN)]) $
-    return ()
+  -- elAttr "script" (Map.fromList [("defer", "defer"), ("src", jqueryCDN)]) $
+  --   return ()
+  -- elAttr "script" (Map.fromList [("defer", "defer"), ("src", popperCDN)]) $
+  --   return ()
   elAttr "script" (Map.fromList [("defer", "defer"), ("src", bootstrapJsCDN)]) $
     return ()
   where
-    jqueryCDN = "https://code.jquery.com/jquery-3.3.1.min.js"
-    popperCDN =
-      "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    -- jqueryCDN =  "https://code.jquery.com/jquery-3.3.1.min.js"
+    -- popperCDN =
+    --    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
     bootstrapJsCDN =
-      "https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.bundle.js"
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 
 svgAut ::
   ( MonadWidget t m,
@@ -88,3 +105,8 @@ svgAut auto = do
         return $ Te.pack $ toString bytes
   svg <- liftIO $ graphvizWithHandle Dot (parseDotGraph $ Tel.pack $ nfaToDot auto :: G.DotGraph String) Svg getData
   void $ elDynHtml' "div" $ return svg
+
+labelledButton :: (MonadWidget t m) => Text -> m (Event t ())
+labelledButton lbl = do
+  (e, _) <- elAttr' "div" ("class" =: "btn btn-primary mx-1") $ text lbl
+  return $ () <$ domEvent Click e
